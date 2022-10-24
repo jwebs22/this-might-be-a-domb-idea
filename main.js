a5d0ac467cda2c71dcb417ae1d135f5f91e9f312
jwebs22 <jenny.webster@codeimmersives.com>
2022-10-24 15:37:48 -0400

    Assignment upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let paragraph = document.querySelector("p");
+paragraph.style.color = "lightblue";
+paragraph.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+let header = document.querySelector("h1");
+header.style.fontSize = "48px";
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = "0.5";
+
+let item3 = document.querySelector("#item-3");
+item3.innerText = "I say, 'Hi!'";
+
+let myImage = document.querySelector("img");
+myImage.src = "https://bit.ly/3F9Oxco";
+myImage.style.height = "300px";
+
+let secondPic = document.querySelector("#secondPic");
+secondPic.src = "https://bit.ly/3smFj54";
+secondPic.style.height = "300px";
+
+let item16 = document.querySelector("#item-16");
+item16.className = "item";
+item16.innerText = "Won't get fooled again.";
